ba4458622d0dcbeca37ac2e24243a150fb84014a
VzqzAlfred <alfred.vzqz96@outlook.com>
2019-11-13 10:19:42 -0600

    servicios img

--- application/views/categoriaPostales.php
+++ application/views/categoriaPostales.php
@@ -14,9 +14,9 @@
                     <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="<?php base_url(); ?>enviarPostales"><img  class="img-thumbnail img-fluid image" id="img1" src="assets/img/factureras/em/aeromexico.jpg"  ></a></div>
                     <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/factureras/em/volkswagen.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/factureras/em/volkswagen.jpg"></a></div>
                     <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/factureras/em/volaris.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/factureras/em/volaris.jpg"></a></div>
-                    <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/jumex.png"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/jumex.png"></a></div>
-                    <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/ibm.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/ibm.jpg"></a></div>
-                    <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/google.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/google.jpg"></a></div>
+                    <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/servicios/banco.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/servicios/banco.jpg"></a></div>
+                    <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/servicios/santander.png"><img class="img-thumbnail img-fluid image" src="assets/img/servicios/santander.png"></a></div>
+                    <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/servicios/bancomer.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/servicios/bancomer.jpg"></a></div>
                     <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/image6.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/image6.jpg"></a></div>
                     <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/image5.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/image5.jpg"></a></div>
                     <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/banco.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/banco.jpg"></a></div>
