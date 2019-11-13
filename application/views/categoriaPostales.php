a2e45bd67de48d709b2c4c3cee4085f0fddee5e2
VzqzAlfred <shadow_fenix96@hotmail.com>
2019-11-13 09:31:51 -0600

    cambio de imagenes de las empresa

--- application/views/categoriaPostales.php
+++ application/views/categoriaPostales.php
@@ -13,13 +13,13 @@
                 <div class="row">
                     <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="<?php base_url(); ?>enviarPostales"><img  class="img-thumbnail img-fluid image" id="img1" src="assets/img/postales/amor-amistad/image1.jpg"  ></a></div>
                     <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/postales/amor-amistad/image2.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/postales/amor-amistad/image2.jpg"></a></div>
+                    <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/volkswagen.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/volkswagen.jpg"></a></div>
+                    <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/jumex.png"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/jumex.png"></a></div>
+                    <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/ibm.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/ibm.jpg"></a></div>
+                    <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/google.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/google.jpg"></a></div>
                     <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/image6.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/image6.jpg"></a></div>
                     <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/image5.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/image5.jpg"></a></div>
-                    <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/image1.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/image1.jpg"></a></div>
-                    <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/image4.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/image4.jpg"></a></div>
-                    <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/image6.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/image6.jpg"></a></div>
-                    <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/image5.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/image5.jpg"></a></div>
-                    <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/image1.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/image1.jpg"></a></div>
+                    <div class="col-md-6 col-lg-4 item"><a class="lightbox" href="assets/img/scenery/banco.jpg"><img class="img-thumbnail img-fluid image" src="assets/img/scenery/banco.jpg"></a></div>
                 </div>
             </div>
         </section>
