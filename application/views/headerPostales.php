033e9d4e776acf113df51fb89674f2c5b979f568
VzqzAlfred <alfred.vzqz96@outlook.com>
2019-11-14 01:02:53 -0600

    agrego cabecera tecnologías

--- application/views/headerPostales.php
+++ application/views/headerPostales.php
@@ -27,7 +27,7 @@
                 <ul class="nav navbar-nav ml-auto">
                     <li class="nav-item" role="presentation"><a class="nav-link" href="<?php base_url(); ?>inicio">Inicio</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link" href="<?php base_url(); ?>caracteristicas">Características</a></li>
-                    <li class="nav-item" role="presentation"><a class="nav-link active" href="<?php base_url(); ?>postales">Empresas</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="<?php base_url(); ?>postales">Empresas</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link" href="<?php base_url(); ?>about">Acerca de</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link" href="<?php base_url(); ?>contacto">Contáctanos</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link" href="<?php base_url(); ?>login">Login</a></li>
@@ -46,6 +46,9 @@
             </li>
             <li class="chapternav-item">
               <a>Mercado</a>
+            </li>
+            <li class="chapternav-item">
+              <a>Tecnologías</a>
             </li>
             <li class="chapternav-item">
               <a>Otro</a>
