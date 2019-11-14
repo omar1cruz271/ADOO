9ac0eaa65041301f8f52496a14f8c1fa2346d8ef
VzqzAlfred <alfred.vzqz96@outlook.com>
2019-11-14 00:30:04 -0600

    tipo de letra

--- application/views/InicioWork.php
+++ application/views/InicioWork.php
@@ -82,7 +82,7 @@
     <section id="sect2" class="clean-block about-us">
         <div class="container">
             <div class="block-heading">
-                <h2 class="text-info">Acerca de nosotros</h2>
+                <h2 class="text-white">Acerca de nosotros</h2>
                 <p>Encuentra información acerca de cada uno de los desarrolladores</p>
             </div>
             <div class="row justify-content-center">
@@ -90,7 +90,7 @@
                     <div class="card clean-card text-center">
                         <div class="card-body info">
                             <h4 class="card-title">Sandra Calderón</h4>
-                            <p class="card-text">Carrera: Ing. Mecanica</p>
+                            <p class="card">Carrera: Ing. Mecanica</p>
                             <div class="icons"><a href="#"><i class="icon-social-facebook"></i></a><a href="#"><i class="icon-social-instagram"></i></a><a href="#"><i class="icon-social-twitter"></i></a></div>
                         </div>
                     </div>
@@ -99,7 +99,7 @@
                     <div class="card clean-card text-center">
                         <div class="card-body info">
                             <h4 class="card-title">Omar Cruz</h4>
-                            <p class="card-text">Carrera: Ing. Sistemas Cmputacionales.</p>
+                            <p class="card">Carrera: Ing. Sistemas Cmputacionales.</p>
                             <div class="icons"><a href="#"><i class="icon-social-facebook"></i></a><a href="#"><i class="icon-social-instagram"></i></a><a href="#"><i class="icon-social-twitter"></i></a></div>
                         </div>
                     </div>
@@ -108,7 +108,7 @@
                     <div class="card clean-card text-center">
                         <div class="card-body info">
                             <h4 class="card-title">Karen Cruz</h4>
-                            <p class="card-text">Carrera: Ing. Comunicaciones y eletrónica.</p>
+                            <p class="card">Carrera: Ing. Comunicaciones y eletrónica.</p>
                             <div class="icons"><a href="#"><i class="icon-social-facebook"></i></a><a href="#"><i class="icon-social-instagram"></i></a><a href="#"><i class="icon-social-twitter"></i></a></div>
                         </div>
                     </div>
--- application/views/about-us.php
+++ application/views/about-us.php
@@ -14,7 +14,7 @@
                     <div class="card clean-card text-center">
                         <div class="card-body info">
                             <h4 class="card-title">Sandra Calderón</h4>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                            <p class="card-text">Carrera: Ing. Mecanica</p>
                             <div class="icons"><a href="#"><i class="icon-social-facebook"></i></a><a href="#"><i class="icon-social-instagram"></i></a><a href="#"><i class="icon-social-twitter"></i></a></div>
                         </div>
                     </div>
@@ -23,7 +23,7 @@
                     <div class="card clean-card text-center">
                         <div class="card-body info">
                             <h4 class="card-title">Omar Cruz</h4>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                            <p class="card-text">Carrera: Ing. Sistemas Computacionales.</p>
                             <div class="icons"><a href="#"><i class="icon-social-facebook"></i></a><a href="#"><i class="icon-social-instagram"></i></a><a href="#"><i class="icon-social-twitter"></i></a></div>
                         </div>
                     </div>
@@ -32,7 +32,7 @@
                     <div class="card clean-card text-center">
                         <div class="card-body info">
                             <h4 class="card-title">Karen Cruz</h4>
-                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                            <p class="card-text">Carrera: Ing. Comunicaciones y electrónica.</p>
                             <div class="icons"><a href="#"><i class="icon-social-facebook"></i></a><a href="#"><i class="icon-social-instagram"></i></a><a href="#"><i class="icon-social-twitter"></i></a></div>
                         </div>
                     </div>
