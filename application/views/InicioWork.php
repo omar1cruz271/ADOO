15d7f52c7f3e035f0a5bc0a9aeace6f1961e4793
VzqzAlfred <alfred.vzqz96@outlook.com>
2019-11-13 19:55:12 -0600

    cursores

--- application/views/InicioWork.php
+++ application/views/InicioWork.php
@@ -74,6 +74,7 @@
                     <li data-target="#carousel-1" data-slide-to="0" class="active"></li>
                     <li data-target="#carousel-1" data-slide-to="1"></li>
                     <li data-target="#carousel-1" data-slide-to="2"></li>
+                    <li data-target="#carousel-1" data-slide-to="3"></li>
                 </ol>
             </div>
         </div>
