91d053002647389ae9f2a3c3176eba85df8ab083
VzqzAlfred <alfred.vzqz96@outlook.com>
2019-11-14 20:08:17 -0600

    letra tamaño

--- application/views/InicioWork.php
+++ application/views/InicioWork.php
@@ -6,9 +6,9 @@
     
     <section class="clean-block clean-hero" style="background-image:url(&quot;assets/img/tech/img1.jpg&quot;);color:rgba(0,0,0,.6);">
         <div class="text">
-            <strong>
+            <br>
                 <i><h1>¡BIENVENIDO!</h1></i>
-            <strong>
+            </br>
             <p>Encuentra las ofertas de trabajo más cercanas a tu localidad.</p>
             <form action="POST"><button class="btn btn-outline-light btn-lg" type="button"  onclick=" location.href='<?php base_url(); ?>login'">Empieza ahora</button></div>
             </form>
